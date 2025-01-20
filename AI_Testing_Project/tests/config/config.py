85020e6ee48cb725a2a573c8f63b6d12d898cfee
Sandra <32596579+sandra1974@users.noreply.github.com>
2025-01-20 12:50:23 +0100

    Add files via upload

---
+++ AI_Testing_Project/tests/config/config.py
@@ -0,0 +1,5 @@
+class Config:
+    dataset_folder = r"C:\Users\SandraDujmovic\Documents\AI_Testing_Projects\ClaudeAI\tests\LLM\Model_Engineering\Benchmarks\Dataset"
+    image_folder = r"C:\Users\SandraDujmovic\Documents\AI_Testing_Projects\ClaudeAI\tests\LLM\Other\Images"
+    pdf_folder = r"C:\Users\SandraDujmovic\Documents\AI_Testing_Projects\ClaudeAI\tests\LLM\Other\Documents"
+   
