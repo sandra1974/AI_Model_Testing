6973f75de470260efa2bcb365610aa13a1cc0e82
Sandra <32596579+sandra1974@users.noreply.github.com>
2025-01-20 12:51:41 +0100

    Update config.py

--- AI_Testing_Project/tests/config/config.py
+++ AI_Testing_Project/tests/config/config.py
@@ -2,4 +2,6 @@ class Config:
     dataset_folder = r"C:\Users\SandraDujmovic\Documents\AI_Testing_Projects\ClaudeAI\tests\LLM\Model_Engineering\Benchmarks\Dataset"
     image_folder = r"C:\Users\SandraDujmovic\Documents\AI_Testing_Projects\ClaudeAI\tests\LLM\Other\Images"
     pdf_folder = r"C:\Users\SandraDujmovic\Documents\AI_Testing_Projects\ClaudeAI\tests\LLM\Other\Documents"
+
+dataset_folder = r"C:\AI_Testing_Project\tests\Model_Engineering\Benchmarks\Dataset"
    
